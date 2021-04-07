f85ac792b6ad49feec72985dd3bb8e9e039f9f82
Olga <zhuravel.olga95@gmail.com>
2021-04-07 23:59:24 +0300

    added script show/hide list in the filters

---
+++ dev/js/main.js
@@ -0,0 +1,34 @@
+
+var parentList = document.querySelectorAll('.list');
+var btnMore = document.querySelectorAll('.btn-more');
+
+function hideList () {
+    parentList.forEach(function (item) {
+        var child = item.children;
+        for (var i = 12; i < child.length; i++) {
+            child[i].classList.add('d-none');
+        }
+    });
+}
+
+hideList();
+
+// function showList () {}
+btnMore.forEach(function (item) {
+    item.addEventListener('click', () => {
+        var siblings = item.previousElementSibling;
+        var child = siblings.children;
+
+        item.classList.toggle('close');
+
+        for (var i = 12; i < child.length; i++) {
+            child[i].classList.toggle('d-none') ;
+            if (item.classList.contains('close')) {
+                item.innerHTML = 'close';
+            } else {
+                item.innerHTML = 'show more';
+            }
+        }
+    });
+});
+
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+var parentList = document.querySelectorAll('.list');
+var btnMore = document.querySelectorAll('.btn-more');
+
+function hideList() {
+  parentList.forEach(function (item) {
+    var child = item.children;
+
+    for (var i = 12; i < child.length; i++) {
+      child[i].classList.add('d-none');
+    }
+  });
+}
+
+hideList(); // function showList () {}
+
+btnMore.forEach(function (item) {
+  item.addEventListener('click', function () {
+    var siblings = item.previousElementSibling;
+    var child = siblings.children;
+    item.classList.toggle('close');
+
+    for (var i = 12; i < child.length; i++) {
+      child[i].classList.toggle('d-none');
+
+      if (item.classList.contains('close')) {
+        item.innerHTML = 'close';
+      } else {
+        item.innerHTML = 'show more';
+      }
+    }
+  });
+});
