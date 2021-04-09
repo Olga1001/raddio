3c5357cd2e94d85f6031e3e8a14e365b37bed2e2
Olga <zhuravel.olga95@gmail.com>
2021-04-10 01:04:49 +0300

    added player

---
+++ dist/js/audio.js
@@ -0,0 +1,31 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  mejs.i18n.language('en');
+  var mediaElements = document.querySelectorAll('video, audio'),
+      i,
+      total = mediaElements.length;
+
+  for (i = 0; i < total; i++) {
+    new MediaElementPlayer(mediaElements[i], {
+      stretching: 'auto',
+      startVolume: '0.8',
+      // audioVolume: 'vertical',
+      success: function success(media) {
+        var renderer = document.getElementById(media.id + '-rendername');
+        media.addEventListener('loadedmetadata', function () {
+          var src = media.originalNode.getAttribute('src').replace('&amp;', '&');
+
+          if (src !== null && src !== undefined) {
+            renderer.querySelector('.src').innerHTML = '<a href="' + src + '" target="_blank">' + src + '</a>';
+            renderer.querySelector('.renderer').innerHTML = media.rendererName;
+            renderer.querySelector('.error').innerHTML = '';
+          }
+        });
+        media.addEventListener('error', function (e) {
+          renderer.querySelector('.error').innerHTML = '<strong>Error</strong>: ' + e.message;
+        });
+      }
+    });
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const script = require('./gulp/tasks/scripts');
 const fonts = require('./gulp/tasks/fonts');
-const vendors = require('./gulp/tasks/vendorsJS');
+// const vendors = require('./gulp/tasks/vendorsJS');
 const imageMinify = require('./gulp/tasks/imageMinify');
 const styles = require('./gulp/tasks/styles');
 const clean = require('./gulp/tasks/clean');
@@ -10,7 +10,7 @@ const spriteSVG = require('./gulp/tasks/spriteSVG');
 const serve = require('./gulp/tasks/serve');
 const spritePNG = require('./gulp/tasks/spritePNG');
 
-const dev = gulp.parallel(pug2html, script, vendors, styles, imageMinify, spriteSVG, spritePNG, fonts);
+const dev = gulp.parallel(pug2html, script, styles, imageMinify, spriteSVG, spritePNG, fonts);
 
 exports.default = gulp.series(
   clean,
